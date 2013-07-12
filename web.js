5ebb081ca64ff0c9c96a053ed089dde5cea030a0
Saugat Lama <lamas14@mycu.concord.edu>
2013-07-12 00:19:39 +0000

    Added index.html and modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = new Buffer(50);
+  buf.write(fs.readFileSync('index.html', function (err, data){if(err) throw err;}), "utf-8");
+  response.send(buf.toString('utf-8', 0, 27);
 });
 
 var port = process.env.PORT || 5000;
